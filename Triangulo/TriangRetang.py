3b97af98cb883f95917927c5a581e53dc978f091
Érick Alves <99452441+ErickNoGit@users.noreply.github.com>
2022-08-18 11:11:01 -0300

    Add files via upload

---
+++ Triangulo/TriangRetang.py
@@ -0,0 +1,9 @@
+# calculo de hipotenusa simples
+import math
+
+a = float(input("disgite um cateto: "))
+b = float(input("digite o segundo cateto: "))
+# hipotenusa
+h = (a**2) + (b**2)
+# mostra a radiação na tela com sqrt da biblioteca math
+print(math.sqrt(h))
